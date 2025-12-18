629b806b3fc03c997b23096a6dc618da6bdd4e78
Pablo J Blasco <pablojblasco@gmail.com>
2025-12-18 20:04:49 +0100

    Improve altitude generation adding some mountain ranges and a continental mascara

--- src/world/Camera.gd
+++ src/world/Camera.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-const SPEED := 400
+const SPEED := 800
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- src/world/TileGenerator.gd
+++ src/world/TileGenerator.gd
@@ -62,8 +62,51 @@ func generate_tile_data(q: float, r:float) -> HexTileData:
 func get_altitude(q: int, r: int) -> float:
 	var x = float(q)
 	var y = float(r)
-	var value = altitude_noise.get_noise_2d(x, y)
-	return (value + 1.0) * 0.5  # [-1,1] → [0,1]
+	var amplitude = 1.0
+	var frequency = 0.5
+	var lacunarity = 1.2
+	var persistence = 0.1
+	var x_offset = noise_seed
+	var y_offset = noise_seed
+	var mountainness = 0.7
+	var sea_level = 0.3
+	var value = 0.0
+	for i in range(8):
+		value += amplitude * altitude_noise.get_noise_2d(frequency * x + x_offset, frequency * y + y_offset)
+		amplitude *= persistence
+		frequency *= lacunarity
+		x_offset += x_offset
+		y_offset += y_offset
+
+	# Add mountain ranges when the point is above the sea level
+	if (value > sea_level):
+		# Random but deterministic [-π,π] value for tilting (rotating the domain)
+		var mountain_tilting = altitude_noise.get_noise_2d(noise_seed, noise_seed) * PI
+		var xp = x*cos(mountain_tilting) - y*sin(mountain_tilting)
+		var yp = x*sin(mountain_tilting) + y*cos(mountain_tilting)
+		var mountranges = (altitude_noise.get_noise_2d(xp*0.5, yp*0.5) + 1.0) * 0.5
+		mountranges = max(mountranges - mountainness, 0) * 1 / (1-mountainness)
+		value += mountranges
+	# Apply mascara to have something like continents
+	var mask = cell_mascara(q,r,x,y)
+	
+	return clamp(mask + (value + 1.0) * 0.5, 0, 1)
+	#return (value + 1.0) * 0.5  # [-1,1] → [0,1]
+	
+func cell_mascara(q:int, r: int, x: float,y: float) -> float:
+	var wavelength = 120
+	var cx = floor(x/wavelength)
+	var cy = floor(y/wavelength)
+	var lx = x - wavelength * (cx + 0.5)
+	var ly = y - wavelength * (cy + 0.5)
+	
+	var n = altitude_noise.get_noise_1d(q) * altitude_noise.get_noise_1d(r) + 0.5*altitude_noise.get_noise_1d(2*q) * 0.5*altitude_noise.get_noise_1d(2*r)
+	lx += 0.2 * wavelength * n
+	ly += 0.2 * wavelength * n
+
+	var dist = sqrt(lx*lx + ly*ly)
+	var base_mask = 0.5 + 0.25 * cos(1.75*PI * dist / wavelength)
+	return max(base_mask, 0) - 0.5
 	
 func get_humidity(q: int, r: int) -> float:
 	var value = humidity_noise.get_noise_2d(float(q), float(r))
